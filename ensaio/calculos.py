a5bdafbed25286db6def8ed350d5f5e29fb784f8
alunocanva25-hub <alunocanva25@gmail.com>
2026-03-21 14:05:17 -0300

    Update calculos.py

--- ensaio/calculos.py
+++ ensaio/calculos.py
@@ -1,1 +1,14 @@
+def calcular_potencia(tensao, corrente, fp):
+    return tensao * corrente * fp
+
+def energia_teorica_wh(potencia, tempo_seg):
+    return potencia * (tempo_seg / 3600.0)
+
+def energia_medida_wh(pulsos, constante):
+    return pulsos * constante
+
+def calcular_erro(energia_medida, energia_teorica):
+    if energia_teorica <= 0:
+        return 0.0
+    return ((energia_medida - energia_teorica) / energia_teorica) * 100.0
 
